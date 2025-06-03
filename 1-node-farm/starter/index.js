762519849e47556957e4ed6a1a24e33d1a07c7f8
Danny Lin <danny.l@ljit.io>
2025-06-03 15:06:42 +0800

    Update

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
+const http = require('http');
 
+///////////////////////////////////////////////////
+// FILES
+///////////////////////////////////////////////////
 // Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
@@ -12,24 +16,35 @@ const fs = require('fs');
 // console.log('File written');
 
 // Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
-	if (error) {
-		console.log(error);
+// fs.readFile('./txt/start.txt', 'utf-8', (error, data1) => {
+// 	if (error) {
+// 		console.log(error);
 
-		return;
-	}
+// 		return;
+// 	}
 
-	fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
-		console.log(data2);
+// 	fs.readFile(`./txt/${data1}.txt`, 'utf-8', (error, data2) => {
+// 		console.log(data2);
 
-		fs.readFile('./txt/append.txt', 'utf-8', (error, data3) => {
-			console.log(data3);
+// 		fs.readFile('./txt/append.txt', 'utf-8', (error, data3) => {
+// 			console.log(data3);
 
-			fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', error => {
-				console.log('Your file has been written');
-			});
-		});
-	});
+// 			fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', error => {
+// 				console.log('Your file has been written');
+// 			});
+// 		});
+// 	});
+// });
+
+// console.log('Will read file');
+
+///////////////////////////////////////////////////
+// SERVER
+///////////////////////////////////////////////////
+const server = http.createServer((req, res) => {
+	res.end('Hello from the server');
 });
 
-console.log('Will read file');
+server.listen(8000, '127.0.0.1', () => {
+	console.log('Listening to requests on port 8000');
+});
