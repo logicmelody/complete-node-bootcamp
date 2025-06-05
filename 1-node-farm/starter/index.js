22b634db2b37faa0a95643e341a3e4e7c9734e02
Danny Lin <danny.l@ljit.io>
2025-06-05 18:05:01 +0800

    Update

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -42,22 +42,36 @@ const url = require('url');
 ///////////////////////////////////////////////////
 // SERVER
 ///////////////////////////////////////////////////
+const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
 	const pathName = req.url;
 
+	// Overview page
 	if (pathName === '/' || pathName === '/overview') {
-		res.end('This is the OVERVIEW');
+		res.writeHead(200, { 'Content-type': 'text/html' });
 
+		const cardsHtml = dataObject.map(item => replaceTemplate(templateCard, item)).join('');
+		const output = templateOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+
+		res.end(output);
+
+	// Product page
 	} else if (pathName === '/product') {
-		res.end('This is the PRODUCT');
+		res.writeHead(200, { 'Content-type': 'text/html' });
+		res.end(templateProduct);
 
+	// API
 	} else if (pathName === '/api') {
 		res.writeHead(200, { 'Content-type': 'application/json' });
 		res.end(data);
 
+	// Not found
 	} else {
 		res.writeHead(404, {
 			'Content-type': 'text/html',
@@ -70,3 +84,20 @@ const server = http.createServer((req, res) => {
 server.listen(8000, '127.0.0.1', () => {
 	console.log('Listening to requests on port 8000');
 });
+
+function replaceTemplate(template, product) {
+	let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+	output = output.replace(/{%IMAGE%}/g, product.image);
+	output = output.replace(/{%PRICE%}/g, product.price);
+	output = output.replace(/{%FROM%}/g, product.from);
+	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+	output = output.replace(/{%QUANTITY%}/g, product.quantity);
+	output = output.replace(/{%DESCRIPTION%}/g, product.description);
+	output = output.replace(/{%ID%}/g, product.id);
+
+	if (!product.organic) {
+		output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+	}
+
+	return output;
+}
