a7aced70cc917c5adf0fec94371acf184e141bde
Danny Lin <danny.l@ljit.io>
2025-06-06 13:33:14 +0800

    Update

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -50,10 +50,13 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-	const pathName = req.url;
+	const {
+		query,
+		pathname,
+	} = url.parse(req.url, true);
 
 	// Overview page
-	if (pathName === '/' || pathName === '/overview') {
+	if (pathname === '/' || pathname === '/overview') {
 		res.writeHead(200, { 'Content-type': 'text/html' });
 
 		const cardsHtml = dataObject.map(item => replaceTemplate(templateCard, item)).join('');
@@ -62,12 +65,15 @@ const server = http.createServer((req, res) => {
 		res.end(output);
 
 	// Product page
-	} else if (pathName === '/product') {
+	} else if (pathname === '/product') {
+		const product = dataObject[query.id];
+		const output = replaceTemplate(templateProduct, product);
+
 		res.writeHead(200, { 'Content-type': 'text/html' });
-		res.end(templateProduct);
+		res.end(output);
 
 	// API
-	} else if (pathName === '/api') {
+	} else if (pathname === '/api') {
 		res.writeHead(200, { 'Content-type': 'application/json' });
 		res.end(data);
 
