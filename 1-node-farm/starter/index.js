5a65b0d94da0b0391cf6ef352ef7ad175f6acb6e
Danny Lin <danny.l@ljit.io>
2025-06-06 13:43:23 +0800

    Update

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate');
+
 ///////////////////////////////////////////////////
 // FILES
 ///////////////////////////////////////////////////
@@ -90,20 +92,3 @@ const server = http.createServer((req, res) => {
 server.listen(8000, '127.0.0.1', () => {
 	console.log('Listening to requests on port 8000');
 });
-
-function replaceTemplate(template, product) {
-	let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-	output = output.replace(/{%IMAGE%}/g, product.image);
-	output = output.replace(/{%PRICE%}/g, product.price);
-	output = output.replace(/{%FROM%}/g, product.from);
-	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-	output = output.replace(/{%QUANTITY%}/g, product.quantity);
-	output = output.replace(/{%DESCRIPTION%}/g, product.description);
-	output = output.replace(/{%ID%}/g, product.id);
-
-	if (!product.organic) {
-		output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-	}
-
-	return output;
-}
